29255a55bda3d801ab2cd0808501dbbaee08a559
eericheva <epifana89@gmail.com>
2024-05-30 13:04:26 +0200

    add V3 with Runnable Sequences

--- start_here.py
+++ start_here.py
@@ -8,14 +8,16 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 
-import prompt_templates
 from create_vectorstore import create_vectorstore
 from setup import Config, logger, print_config
-from tools.documents_manipulations import get_unique_union
+from tools import prompt_templates
 from tools.invoke_result import (
+    invoke_generate_queries_with_origin,
     invoke_input_context_answer,
     invoke_query_source_documents_result,
+    invoke_unique_docs_union_from_retriever,
 )
 
 
@@ -79,26 +81,55 @@ def overview():
     result = chain.invoke({"query": Config.MYQ})
     print(invoke_query_source_documents_result(result))
 
-    #### V2 ####
-    logger.info("Multi Query RETRIEVER")
-    # # Prompt
-    prompt = PromptTemplate(
+    #### V3 with Runnable Sequences ####
+    # Generate multiple alternatives to the question formulation
+    # Prompt for multiple alternatives to the question formulation
+    prompt_multi_query = PromptTemplate(
         template=prompt_templates.prompt_multi_query,
-        input_variables=["question", "question_numbers"],
+        input_variables=["question", "number_questions"],
     )
-    generate_queries = prompt | llm | StrOutputParser() | (lambda x: x.split("\n"))
-    # Retrieve
-    retrieval_chain = generate_queries | retriever.map() | get_unique_union
-    # docs = retrieval_chain.invoke({"question": Config.MYQ})
+    # Chain for generating multiple alternatives to the question formulation
+    generate_queries_chain = (
+        {
+            "question": itemgetter("question"),
+            "question_numbers": itemgetter("question_numbers"),
+        }
+        | prompt_multi_query
+        | llm
+        | StrOutputParser()
+    )
+    invoke_generate_queries_chain = (
+        {"question": itemgetter("question"), "alternatives": generate_queries_chain}
+        | RunnableLambda(invoke_generate_queries_with_origin)
+        | (lambda x: x.split("\n"))
+    )
+    # to check multiple generated questions:
+    # result = invoke_generate_queries_chain.invoke({"question": Config.MYQ, "question_numbers": 2})
+    # print(result)
 
-    logger.info("RAG CHAIN")
+    # Retrieval Chain for multiple alternatives to the question formulation
+    retrieval_chain = (
+        invoke_generate_queries_chain
+        | retriever.map()
+        | invoke_unique_docs_union_from_retriever
+    )
+    # to check list of retrieved documents
+    # result = retrieval_chain.invoke({"question": Config.MYQ, "question_numbers": 2})
+    # print(result)
+
+    # Prompt for generation answer with retriever and generatin prompt
+    prompt_generation = PromptTemplate(
+        template=prompt_templates.prompt_template_question_context,
+        input_variables=["question", "context"],
+    )
+    # RAG Chain
     rag_chain = (
         {
             "context": retrieval_chain,
             "question": itemgetter("question"),
-            "question_numbers": itemgetter("question_numbers"),
+            # "question_numbers": itemgetter("question_numbers"),
         }
-        | prompt
+        | prompt_generation
         | llm
         | StrOutputParser()
     )
--- tools/prompt_templates.py
+++ tools/prompt_templates.py
@@ -12,6 +12,20 @@ Question: {input}
 *** Helpful Answer***:
 """
 
+prompt_template_question_context = """
+Use the following pieces of context to answer the question at the end.
+Please follow the following rules:
+1. If you don't know the answer, don't try to make up an answer.
+Just say "I can't find the final answer but you may want to check the following links".
+2. If you find the answer, write the answer in a concise way with five sentences maximum.
+
+{context}
+
+Question: {question}
+
+*** Helpful Answer***:
+"""
+
 prompt_template_question = """
 Use the following pieces of context to answer the question at the end.
 Please follow the following rules:
@@ -29,4 +43,7 @@ prompt_multi_query = """You are an AI language model assistant. Your task is to 
 different versions of the given user question to retrieve relevant documents from a vector
 database. By generating multiple perspectives on the user question, your goal is to help
 the user overcome some of the limitations of the distance-based similarity search.
-Provide these alternative questions separated by newlines. Original question: {question}"""
+Provide these alternative questions separated by newlines.
+
+Original question: {question}
+"""
